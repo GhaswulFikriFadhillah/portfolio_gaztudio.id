7cb4e1d4f2085b1db2280006b1d0da97b5163c5b
GhaswulFikriFadhillah <ghaswul23ti@mahasiswa.pcr.ac.id>
2025-12-22 03:46:31 +0700

    Siap deploy ke github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Tambahkan baris di bawah ini. Pastikan tulisannya persis sama:
+  base: "/portfolio_gaztudio.id/",
 })
